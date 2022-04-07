d4ec6682fb4cfeae5dae011f68d2c3c737e959e1
hnn@bbi-biotech.com <hnn@bbi-biotech.com>
2022-04-07 09:37:18 +0200

    purchase_order_mod

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# bbi Customisation für Freitextnotizen in Angebotspositionen
+# @author: Tilo K
+# @date:    April 2022
+# Version 1.0 Webshopeigenschaft an product_template, Positionsfreitext im angebot vom Produkt
+
+{
+  'name': 'bbi mod purchase',
+  'version': '1.0',
+  'category': 'Purchase',
+  'description': """
+     verändert Methode _add_supplier_to_product Methode, sodass alle Einkäuf eingetragen werden
+    """,
+  'depends': [
+    'base',
+    'purchase',
+  ],
+  'data': [
+  ,
+  ],
+}
