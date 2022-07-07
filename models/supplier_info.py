3eddb7e09a0e65a2d0b59e9b50cc1643cc3de14f
Tilo <tilo.karczewski@gmx>
2022-07-07 13:55:02 +0200

    standardlieferant im product form-supplier-tab kann getauscht werden

--- models/supplier_info.py
+++ models/supplier_info.py
@@ -2,6 +2,13 @@ from odoo import api, fields, models
 
 class SupplierInfo(models.Model):
     _inherit = 'product.supplierinfo'
-
+    #tauscht die supplierinfo in der reihenfolge
     def action_open_make_standard(self):
-        print("was mit sequenzen")
+        if self._context.get('sequence_action_clicked_id'):
+            me = self.env['product.supplierinfo'].search([('id','=',self._context.get('sequence_action_clicked_id')),])
+            toSwap = self.env['product.supplierinfo'].search([('product_tmpl_id','=',me.product_tmpl_id.id),('sequence','=',1)])
+            if len(toSwap) > 0:
+                tmp=me.sequence
+                print("swapping {} - {}".format(me.sequence,toSwap.sequence))
+                me.update({'sequence' : 1})
+                toSwap.update({'sequence' : tmp})
