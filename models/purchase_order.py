734aab00a0d6029fb22157ea71577e794f8ef814
hnn@bbi-biotech.com <hnn@bbi-biotech.com>
2022-07-11 10:51:51 +0200

    import error fix

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,4 +1,19 @@
+from datetime import datetime, time
+from dateutil.relativedelta import relativedelta
+from functools import partial
+from itertools import groupby
+import json
+
+from markupsafe import escape, Markup
+from pytz import timezone, UTC
+from werkzeug.urls import url_encode
+
 from odoo import api, fields, models, _
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang, format_amount
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
