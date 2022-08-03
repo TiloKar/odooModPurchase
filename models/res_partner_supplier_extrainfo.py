ce318278a707b6ab9d6fc9cb81abeb5ce1f8ddbf
hnn@bbi-biotech.com <hnn@bbi-biotech.com>
2022-08-03 10:02:23 +0200

    Lieferantennummer generieren

--- models/res_partner_supplier_extrainfo.py
+++ models/res_partner_supplier_extrainfo.py
@@ -1,8 +1,9 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    
+
     supplier_extrainfo = fields.Char('Steuernr. Finanzamt',
         help='bbi_steuernr_finanzamt',
         store=True,
@@ -12,3 +13,17 @@ class ResPartner(models.Model):
         help='bbi_Kundennummer_vom_Lieferanten',
         store=True,
         readonly=False)
+
+    def generateSupplierRef(self):
+        if self.ref is False:
+            result = self.env['res.partner'].search([('supplier_rank', '!=', False)])
+            highest= 0
+            try:
+                for i in result:
+                    if int(i.ref) > highest:
+                        highest = int(i.ref)
+            except:
+                raise ValidationError ('Lieferantennummer beim Kontakt ' + i.name + ' überarbeiten!' )
+
+            highest=highest+1
+            return super(models.Model,self).write({'ref' : str(highest)})
